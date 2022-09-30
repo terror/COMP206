4c41641b55ec853ada10143b36e72f46d47ee23b
Liam <liam@scalzulli.com>
2022-09-30 11:15:05 -0400

    [A2] Add missing functionality

--- Assignments/A2/namefix.bash
+++ Assignments/A2/namefix.bash
@@ -7,7 +7,7 @@
 PROG='/home/2013/jdsilv2/206/mini2/namefix'
 
 err() {
-  echo -e "\e[31merror\e[0m: $1" && exit "$2"
+  echo "error: $1" && exit "$2"
 }
 
 out() {
@@ -15,28 +15,27 @@ out() {
 }
 
 run() {
-  if [[ ! -f $2 ]]; then touch "$2"; fi 
-  $PROG "$1" "$2"
+  $PROG "$1" "$2" && cat "$2"
 }
 
 if [[ $# -ne 2 ]]; then
   out 'Usage: namefix.bash <inputfile> <outputfile>' 1
 fi
 
-if [[ ! -f $1 || ! -r $1 ]]; then
-  err 'Input file must exist, be a file, and be readable' 3
+if [[ ! -f $1 ]]; then
+  err 'Input file must be an existing file' 3
 fi
 
-if [[ $1 -ef $2 ]]; then 
+if [[ ! -r $1 ]]; then
+  err 'Input file must be readable' 3
+fi
+
+if [[ $1 -ef $2 || $2 -ef $PWD ]]; then 
   err 'Input and output files must be different' 2
 fi 
 
-if [[ -f $2 || -d $2 && ! -w $2 ]]; then
+if [[ (-f $2 || -d $2) && ! -w $2 ]]; then
   err 'Output file or directory must be writeable' 4
-fi
-
-if [[ -d $2 && $2 -ef $PWD ]]; then
-  err 'Output directory must differ from the current working directory' 1
 fi
 
 if [[ -d $2 ]]; then
--- Assignments/A2/primechk.bash
+++ Assignments/A2/primechk.bash
@@ -4,28 +4,19 @@
 # Department: Computer Science
 # Email:      liam.scalzulli@mail.mcgill.ca
 
+FILE=
+IFS=$'\n'
+LARGEST=false
+MAX_PRIME=-1
 PROG='/home/2013/jdsilv2/206/mini2/primechk'
-MIN=1
-MAX=1000000000000000000
 
 err() {
-  echo -e "\e[31merror\e[0m: $1" && exit "$2"
+  echo "error: $1" && exit "$2"
 }
 
 out() {
   echo "$1" && exit "$2"
 }
-
-is_valid_number() {
-  if [[ $line =~ ^[0-9]+$ && $line -ge $MIN && $line -le $MAX ]]; then
-    return 0
-  else
-    return 1
-  fi
-}
-
-FILE=
-LARGEST=false
 
 while [[ $# -gt 0 ]]; do
   case $1 in
@@ -38,8 +29,7 @@ while [[ $# -gt 0 ]]; do
       LARGEST=true
       shift
       ;;
-    *)
-     out 'Usage: primechk.bash -f <numbersfile> [-l]' 1;;
+    *) out 'Usage: primechk.bash -f <numbersfile> [-l]' 1;;
   esac
 done
 
@@ -48,15 +38,11 @@ if [[ -z $FILE ]]; then
 fi
 
 if [[ ! -f $FILE ]]; then
-  err 'Input file must exist' 1
+  err 'Input file must exist' 2
 fi
 
-OLDIFS=$IFS
-IFS=$'\n'
-MAX_PRIME=-1
-
-for line in $(cat "$FILE"); do
-  if is_valid_number "$line" && $PROG "$line" &>/dev/null; then
+for line in $(grep -E '^[1-9][0-9]{0,16}' "$FILE"); do
+  if $PROG "$line" &>/dev/null ; then
     if [[ $LARGEST = true && "$line" -gt $MAX_PRIME ]]; then
       MAX_PRIME="$line"
     else
@@ -65,4 +51,10 @@ for line in $(cat "$FILE"); do
   fi
 done
 
-if [[ $LARGEST && $MAX_PRIME -ne -1 ]]; then echo "$MAX_PRIME"; fi
+if [[ $LARGEST = true ]]; then
+  if [[ $MAX_PRIME -ne -1 ]]; then
+    echo "$MAX_PRIME"
+  else
+    err 'Did not find any prime numbers' 3
+  fi
+fi
