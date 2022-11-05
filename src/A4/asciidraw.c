58452f61e06f066557245419eab15a93f74e113b
Liam <liam@scalzulli.com>
2022-11-05 10:57:14 -0400

    [A4] Add error handling, lex arguments properly

--- src/A4/asciidraw.c
+++ src/A4/asciidraw.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
 #include <limits.h>
@@ -9,6 +10,7 @@ enum Command {
   CHAR,
   CIRCLE,
   DISPLAY,
+  END,
   GRID,
   INVALID,
   LINE,
@@ -18,25 +20,29 @@ enum Command {
 const static struct {
   enum Command command;
   const char *str;
-} command_string [] = {
+} COMMAND_STRING [] = {
   {CHAR, "CHAR"},
   {CIRCLE, "CIRCLE"},
   {DISPLAY, "DISPLAY"},
+  {END, "END"},
   {GRID, "GRID"},
   {LINE, "LINE"},
   {RECTANGLE, "RECTANGLE"}
 };
 
 enum Command command_from_string(const char *str) {
-  for (int i = 0; i < sizeof(command_string) / sizeof(command_string[0]); ++i)
-    if (!strcmp(str, command_string[i].str))
-      return command_string[i].command;
+  int size = sizeof(COMMAND_STRING) / sizeof(COMMAND_STRING[0]);
+
+  for (int i = 0; i < size; ++i)
+    if (!strcmp(str, COMMAND_STRING[i].str))
+      return COMMAND_STRING[i].command;
+
   return INVALID;
 }
 
 struct Operation {
   enum Command cmd;
-  int *args;
+  int args[MAX_ARGS];
 };
 
 struct Grid {
@@ -44,6 +50,7 @@ struct Grid {
   char character;
   int width;
   int height;
+  bool initialized;
 };
 
 void character(struct Grid *grid, int args[]) {
@@ -52,19 +59,45 @@ void character(struct Grid *grid, int args[]) {
 
 void circle(struct Grid *grid, int args[]) {
   int x = args[0], y = args[1], radius = args[2];
+
+  if (!grid->initialized) {
+    printf("error: Grid isn't initialized\n");
+    return;
+  }
+
   printf("x: %d, y: %d, radius: %d\n", x, y, radius);
 }
 
 void display(struct Grid grid) {
-  for (int i = 0; i < grid.width; ++i) {
-    for (int j = 0; j < grid.height; ++j)
+  int wrap = 10;
+
+  if (!grid.initialized) {
+    printf("error: Grid isn't initialized\n");
+    return;
+  }
+
+  for (int i = 0; i < grid.height; ++i) {
+    printf("%d ", ((9 - i) % wrap + wrap) % wrap);
+    for (int j = 0; j < grid.width; ++j)
       printf("%c", grid.state[i][j]);
     printf("\n");
   }
+
+  printf(" ");
+
+  for (int i = 0; i < grid.width; ++i)
+    printf("%d", ((9 - i) % wrap + wrap) % wrap);
+
+  printf("\n");
 }
 
 void grid(struct Grid *grid, int args[]) {
   int width = args[0], height = args[1];
+
+  if (grid->initialized) {
+    printf("error: Grid has already been initialized\n");
+    return;
+  }
 
   grid->state = (char**)malloc(sizeof(char*)*width);
 
@@ -73,19 +106,32 @@ void grid(struct Grid *grid, int args[]) {
 
   for (int i = 0; i < width; ++i)
     for (int j = 0; j < height; ++j)
-      grid->state[i][j] = grid->character;
+      grid->state[i][j] = ' ';
 
   grid->width = width;
   grid->height = height;
+  grid->initialized = true;
 }
 
 void line(struct Grid *grid, int args[]) {
   int x1 = args[0], y1 = args[1], x2 = args[2], y2 = args[3];
+
+  if (!grid->initialized) {
+    printf("error: Grid isn't initialized\n");
+    return;
+  }
+
   printf("x1: %d, y1: %d, x2: %d, y2: %d\n", x1, y1, x2, y2);
 }
 
 void rectangle(struct Grid *grid, int args[]) {
   int x1 = args[0], y1 = args[1], x2 = args[2], y2 = args[3];
+
+  if (!grid->initialized) {
+    printf("error: Grid isn't initialized\n");
+    return;
+  }
+
   printf("x1: %d, y1: %d, x2: %d, y2: %d\n", x1, y1, x2, y2);
 }
 
@@ -99,21 +145,28 @@ void read(struct Lexer *lexer) {
 }
 
 struct Operation lex(struct Lexer lexer) {
-  char *token = strtok(lexer.line, " ");
+  char *inner, *outer;
 
   struct Operation operation;
 
+  char *token = strtok_r(lexer.line, " ", &outer);
+
   operation.cmd = command_from_string(token);
 
-  int index = 0, args[MAX_ARGS];
+  int index = 0;
 
-  while (1) {
-    token = strtok(NULL, " ");
-    if (token == NULL) break;
-    args[index++] = atoi(token);
+  for (;;) {
+    token = strtok_r(NULL, " ", &outer);
+
+    if (!token) break;
+
+    char *local = strtok_r(token, ",", &inner);
+
+    while (local) {
+      operation.args[index++] = atoi(local);
+      local = strtok_r(NULL, ",", &inner);
+    }
   }
-
-  operation.args = args;
 
   return operation;
 }
@@ -135,11 +188,13 @@ void eval(struct Interpreter *i) {
     case CIRCLE:
       circle(&i->grid, i->op.args);
       break;
-    case GRID:
-      grid(&i->grid, i->op.args);
-      break;
     case DISPLAY:
       display(i->grid);
+      break;
+    case END:
+      exit(0);
+    case GRID:
+      grid(&i->grid, i->op.args);
       break;
     case INVALID:
       printf("error: Invalid operation\n");
@@ -157,6 +212,7 @@ int main() {
   struct Grid grid;
 
   grid.character = '*';
+  grid.initialized = false;
 
   struct Lexer lexer;
   struct Interpreter interpreter;
@@ -175,5 +231,6 @@ int main() {
 
     // Evaluate the currently loaded operation
     eval(&interpreter);
+
   }
 }
